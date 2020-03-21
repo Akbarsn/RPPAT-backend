88dc3a57ce485ad6821a110848055ee3d49d305e
akbarsn <new.akbarsatya@gmail.com>
2020-03-21 20:35:35 +0700

    Added Middleware for Auth

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authorization = require('./auth')
+const {isAuthenticated} = require('../middleware')
 
 router.use('/auth', authorization)
+
 
 router.get('/fail-login', (req,res)=>{
     res.status(406).json({
