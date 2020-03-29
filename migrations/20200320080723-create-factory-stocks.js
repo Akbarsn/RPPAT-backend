d311535c62fe0f914cdf13b90ddf73779cb12477
Akbar Nugraha <new.akbarsatya@gmail.com>
2020-03-29 17:13:35 +0700

    Fixing factory Stocks migration

--- migrations/20200320080723-create-factory-stocks.js
+++ migrations/20200320080723-create-factory-stocks.js
@@ -26,6 +26,9 @@ module.exports = {
       owner: {
         type: Sequelize.INTEGER
       },
+      type:{
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
